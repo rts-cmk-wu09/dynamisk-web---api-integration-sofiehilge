a1b75d47f203e1b72d5c7f591900b5c8865138e5
sofiehilge <sofie.hilge@gmail.com>
2023-03-16 11:13:03 +0100

    finale styling

--- details.js
+++ details.js
@@ -8,9 +8,11 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${namePoke}`)
 .then((response) => response.json())
 .then((pokemonData) => {
     document.querySelector("body").innerHTML += `
-    <figure class="cards card">
+    <figure class="cards card details">
+    <div class="details-data">
         <h2 class="card-title">${pokemonData.forms[0].name}
-        <img class="card-img" src ="${pokemonData.sprites.front_default}"/>
+        <img class="card-img details-img" src ="${pokemonData.sprites.front_default}"/>
+        </div>
     </figure>
     `
     ;
